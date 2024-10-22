7a0ae8483f09a0a0a41891b70b22616f206b9eec
Varun <tilkireddy@gmail.com>
2024-10-23 00:38:33 +0530

    added html for problem detail, post request is incomplete

--- solveproblems/views.py
+++ solveproblems/views.py
@@ -1,21 +1,44 @@
 import json
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import ProblemForm
 from django.views.decorators.csrf import csrf_exempt
+from .models import Problem
+from submit.views import run_code
 # Create your views here.
 
 def problems_list(request):
+    problems = Problem.objects.all()
+    print(problems[0].description)
+    context = {
+        'problems':problems,
+    }
     template = loader.get_template('all_problems.html')
-    context = {}
+   
     return HttpResponse(template.render(context,request))
 
 
 def problem_detail(request, problem_id):
+    
+    problem = Problem.objects.get(id=problem_id)
+    if request.method == 'POST':
+        # Capture the user's submitted code
+        user_code = request.POST.get('code')
+        # Here you can pass the `user_code` to a function for processing
+        result = run_code(user_code)  # Assume you have a function `run_code` to process the code
+        print(result)
+        # Render the template with the result or any feedback
+        # return render(request, 'problem_detail.html', {
+        #     'problem': problem,
+        #     'result': result,  # Pass the result back to the template if needed
+        # })
+    
+    
+    
+    context = {"problem":  problem}
+
     template = loader.get_template('problem_detail.html')
-    context = {}
     return HttpResponse(template.render(context,request))
 
 
